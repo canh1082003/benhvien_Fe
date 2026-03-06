6e6a3b6f25b302ebb6d775878535dc1bfe3b05b9
Nguyen Cong Anh <nguyenconganh@MacBook-Air-cua-Nguyen-3.local>
2026-03-06 11:59:16 +0700

    Update AdminDashboard KPI endpoint

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from 'react';
-
-import axios from 'axios';
 import { 
   BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer,
   PieChart, Pie, Cell
